d606d8780553605143b40f2eca47a8654becdffc
João <joaokavalciuki@hotmail.com>
2024-05-06 14:42:40 -0300

    Função: Método post implementado

--- src/main/java/com/mongoproject/compassmongo/controllers/UserController.java
+++ src/main/java/com/mongoproject/compassmongo/controllers/UserController.java
@@ -5,11 +5,10 @@ import com.mongoproject.compassmongo.dto.UserDTO;
 import com.mongoproject.compassmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -23,6 +22,15 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDTO dto){
+        User user = userService.insert(dto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 
     @GetMapping
