0d2bd551ee34a233d3103619339708430460f04c
João <joaokavalciuki@hotmail.com>
2024-05-06 12:06:54 -0300

    Projeto iniciado

---
+++ src/main/java/com/mongoproject/compassmongo/CompassMongoApplication.java
@@ -0,0 +1,13 @@
+package com.mongoproject.compassmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompassMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompassMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mongoproject/compassmongo/CompassMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongoproject.compassmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompassMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
