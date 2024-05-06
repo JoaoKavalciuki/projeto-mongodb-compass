37b15e2e9eef4771ee76a280f53fd971e2ef3f23
João <joaokavalciuki@hotmail.com>
2024-05-06 15:32:53 -0300

    Falha: adicionado get e setters

--- src/main/java/com/mongoproject/compassmongo/controllers/exceptions/StandardError.java
+++ src/main/java/com/mongoproject/compassmongo/controllers/exceptions/StandardError.java
@@ -19,4 +19,44 @@ public class StandardError implements Serializable {
         this.message = message;
         this.path = path;
     }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
