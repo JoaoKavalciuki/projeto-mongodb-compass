c0522f6228af3e15f01fd410721e54372bd29904
João <joaokavalciuki@hotmail.com>
2024-05-06 13:59:47 -0300

    Função: Método GET findById implementado

--- src/main/java/com/mongoproject/compassmongo/controllers/UserController.java
+++ src/main/java/com/mongoproject/compassmongo/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.mongoproject.compassmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,10 @@ public class UserController {
     public ResponseEntity<List<UserDTO>> findAll(){
 
         return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        return ResponseEntity.ok(userService.findById(id));
     }
 }
