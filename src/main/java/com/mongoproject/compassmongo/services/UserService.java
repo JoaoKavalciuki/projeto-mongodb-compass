b63d9bba9d1615ec469cd3dae4e2efbd93fc89db
João <joaokavalciuki@hotmail.com>
2024-05-06 13:55:56 -0300

    Função: Método findById implementado

--- src/main/java/com/mongoproject/compassmongo/services/UserService.java
+++ src/main/java/com/mongoproject/compassmongo/services/UserService.java
@@ -3,11 +3,13 @@ package com.mongoproject.compassmongo.services;
 import com.mongoproject.compassmongo.domain.User;
 import com.mongoproject.compassmongo.dto.UserDTO;
 import com.mongoproject.compassmongo.repositories.UserRepository;
+import com.mongoproject.compassmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,6 +26,15 @@ public class UserService {
         var users = userRepository.findAll();
         List<UserDTO> dtos = users.stream().map(user -> new UserDTO(user.getName(), user.getEmail())).toList();
         return dtos;
+    }
 
+    public UserDTO findById(String id){
+        Optional<User> user = userRepository.findById(id);
+
+        if(user.isPresent()){
+            UserDTO dto = new UserDTO(user.get().getName(), user.get().getEmail());
+            return dto;
+        }
+        throw new ObjectNotFoundException("Objeto não encontrado");
     }
 }
