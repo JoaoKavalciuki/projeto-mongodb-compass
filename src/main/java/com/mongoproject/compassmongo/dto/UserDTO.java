551081068fbc15a29ec4b42dd641d19867b87d66
João <joaokavalciuki@hotmail.com>
2024-05-06 13:20:54 -0300

    Refatoração: implementação do Serializable realizada

--- src/main/java/com/mongoproject/compassmongo/dto/UserDTO.java
+++ src/main/java/com/mongoproject/compassmongo/dto/UserDTO.java
@@ -1,4 +1,6 @@
 package com.mongoproject.compassmongo.dto;
 
-public record UserDTO (String name, String email){
+import java.io.Serializable;
+
+public record UserDTO (String name, String email) implements Serializable {
 }
