9876625ad6d2f108b14ae7bad070a9e8ac5f7744
João <joaokavalciuki@hotmail.com>
2024-05-07 15:30:23 -0300

    Função: Query para retornar posts pelo nome do autor implementada

--- src/main/java/com/mongoproject/compassmongo/repositories/PostRepository.java
+++ src/main/java/com/mongoproject/compassmongo/repositories/PostRepository.java
@@ -3,11 +3,14 @@ package com.mongoproject.compassmongo.repositories;
 import com.mongoproject.compassmongo.domain.Post;
 import com.mongoproject.compassmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+    @Query("{'author':  {$regex:  ?0, $options: 'i'}}")
+    List<Post> searchByOwnerName(String name);
     List<Post> findByTitleContainingIgnoreCase(String title);
 }
